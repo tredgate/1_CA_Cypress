709662db7d3bc9d4dcc4d5c64da3e65e051409b1
Petr Fifka <petr.fifka@tredgate.cz>
2023-03-20 16:13:53 +0100

    first lecture Cypress

---
+++ cypress/e2e/learning/first-tests/first_tests.cy.js
@@ -0,0 +1,5 @@
+describe("First Tests", () => {
+  it("Open Tredgate.cz page", () => {
+    cy.visit("https://tredgate.cz/");
+  });
+});
---
+++ cypress/e2e/prep/basics/operating_with_cypress_tests.cy.js
@@ -0,0 +1,13 @@
+describe("Operating with Cypress Tests", () => {
+  it.only("Login Test", () => {
+    cy.visit("http://tredgate.com/pmtool");
+    cy.get("#username").type("fifka_petr");
+    cy.get("#password").type("Tredgate2023");
+    cy.get(".btn").click();
+  });
+
+  it("Test failure - element not present", () => {
+    cy.visit("http://tredgate.com/pmtool");
+    cy.get("#username2");
+  });
+});
